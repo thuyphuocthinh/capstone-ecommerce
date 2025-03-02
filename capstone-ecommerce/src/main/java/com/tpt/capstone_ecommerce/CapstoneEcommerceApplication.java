a1e014911818f59efb6b542f70f767837ec4e50b
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2025-03-02 21:01:39 +0700

    init project

---
+++ capstone-ecommerce/src/main/java/com/tpt/capstone_ecommerce/CapstoneEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.tpt.capstone_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneEcommerceApplication.class, args);
+	}
+
+}
---
+++ capstone-ecommerce/src/test/java/com/tpt/capstone_ecommerce/CapstoneEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpt.capstone_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
